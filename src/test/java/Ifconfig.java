801540d13b2e7f8a30dd298990dc016abfa30787
yanming_dai <shuaizhuaidym@sina.com>
2022-01-16 21:11:49 +0800

    add attack test code

--- src/test/java/Ifconfig.java
+++ src/test/java/Ifconfig.java
@@ -1,32 +1,51 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
-public class Hacker {
-	static {
-		System.out.println("Dangerouse hadker in.");
+public class Ifconfig {
+	static String liner = System.lineSeparator();
+
+	static{
+
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new File("/tmp/hacker.log"));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
 		Runtime runtime = Runtime.getRuntime();
 		try {
 			Process process = runtime.exec("ifconfig");
 			BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			BufferedReader stderrReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 			String line;
-			System.out.println("OUTPUT");
+			pw.write("[OUTPUT]:"+liner);
 			while ((line = stdoutReader.readLine()) != null) {
-				System.out.println(line);
+				pw.write(line);
+				pw.write(liner);
 			}
-			System.out.println("ERROR");
+			pw.write("[ERROR]:"+liner);
 			while ((line = stderrReader.readLine()) != null) {
-				System.out.println(line);
+				pw.write(line);
+				pw.write(liner);
 			}
 			int exitVal = process.waitFor();
-			System.out.println("process exit value is " + exitVal);
+			pw.write("process exit value is " + exitVal);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		} finally {
+			if (pw != null) {
+				pw.flush();
+				pw.close();
+			}
 		}
-	
+
 	}
 
 }
